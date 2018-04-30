a09cb99a1b84ec7ee5a318a913ca0d3cfd5b11f5
themester <dgrripoll@gmail.com>
2018-04-30 20:09:53 +0000

    Added error caused when ID have not been provided

--- errors.go
+++ errors.go
@@ -2,9 +2,14 @@ package goinsta
 
 import "errors"
 
-// ErrNotFound is returned if the request responds with a 404 status code
-// i.e a non existent user
-var ErrNotFound = errors.New("The specified data wasn't found.")
+var (
+	// ErrNotFound is returned if the request responds with a 404 status code
+	// i.e a non existent user
+	ErrNotFound = errors.New("The specified data wasn't found.")
 
-// ErrLoggedOut is returned if the request responds with a 400 status code
-var ErrLoggedOut = errors.New("The account is logged out")
+	// ErrLoggedOut is returned if the request responds with a 400 status code
+	ErrLoggedOut = errors.New("The account is logged out")
+
+	// ErrNoID
+	ErrNoID = errors.New("User id have not being specified. See the documentation at godoc.org")
+)
