3006c5980de446ac5ae8b91bb3ac430213e7157e
themester <dgrripoll@gmail.com>
2018-04-30 21:56:23 +0000

    Deleted uneeded types

--- types.go
+++ types.go
@@ -294,38 +294,6 @@ type CommentResponse struct {
 	Type         int    `json:"type"`
 }
 
-// MediaComments struct for get array of comments of a media
-type MediaComments struct {
-	StatusResponse
-	StringPagination
-	CommentLikesEnabled bool              `json:"comment_likes_enabled"`
-	Comments            []CommentResponse `json:"comments"`
-}
-
-// MediaLikers struct for get array of users that like a media
-type MediaLikers struct {
-	StatusResponse
-	UserCount int    `json:"user_count"`
-	Users     []User `json:"users"`
-}
-
-// ProfileUserResponse struct is current logged in user profile data
-// It's very similar to User struct but have more features
-// Gender -> 1 male , 2 female , 3 unknown
-type ProfileUserResponse struct {
-	User
-	//Birthday -> what the hell is ?
-	PhoneNumber             string           `json:"phone_number"`
-	HDProfilePicVersions    []ImageCandidate `json:"hd_profile_pic_versions"`
-	Gender                  int              `json:"gender"`
-	ShowConversionEditEntry bool             `json:"show_conversion_edit_entry"`
-	ExternalLynxURL         string           `json:"external_lynx_url"`
-	Biography               string           `json:"biography"`
-	HDProfilePicURLInfo     ImageCandidate   `json:"hd_profile_pic_url_info"`
-	Email                   string           `json:"email"`
-	ExternalURL             string           `json:"external_url"`
-}
-
 // ProfileDataResponse have StatusResponse and ProfileUserResponse
 type ProfileDataResponse struct {
 	StatusResponse
@@ -1140,16 +1108,6 @@ type ExploreItem struct {
 			} `json:"user"`
 		} `json:"tray"`
 	} `json:"stories"`
-}
-
-// MediaInfo contains media information
-type MediaInfo struct {
-	Status              string `json:"status"`
-	NumResults          int    `json:"num_results"`
-	AutoLoadMoreEnabled bool   `json:"auto_load_more_enabled"`
-	Items               []Item `json:"items"`
-	MoreAvailable       bool   `json:"more_available"`
-	CommentLikesEnabled bool   `json:"comment_likes_enabled"`
 }
 
 // UserFriendShipResponse is about user_friend_ship response
