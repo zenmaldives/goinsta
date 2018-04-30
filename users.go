3e235cf35814677e462c311f44bf8bdb3ccd77c2
themester <dgrripoll@gmail.com>
2018-04-30 20:10:36 +0000

    Created users array to get list of users

--- users.go
+++ users.go
@@ -1,13 +1,26 @@
 package goinsta
 
-// Users represents instagram users
+// Users represents instagram list of users
 type Users struct {
 	insta  *Instagram
 	cursor int64
 
-	Current *User
+	StatusResponse
+	BigList   bool   `json:"big_list"`
+	Users     []User `json:"users"`
+	PageSize  int    `json:"page_size"`
+	NextMaxID string `json:"next_max_id"`
 }
 
-func (user *Users) Next() bool {
+func NewUsers(insta *Instagram) *Users {
+	users := &Users{
+		insta: insta,
+	}
+	return users
+}
+
+// Next does not keep last data.
+// To keep last data use AddNext.
+func (users *Users) Next() bool {
 	// TODO
 }
