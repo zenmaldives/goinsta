3f07e2e41ec93c9d3ad14682c9b5f56dd987feb1
themester <dgrripoll@gmail.com>
2018-04-30 21:55:36 +0000

    Changed media fields to objects

--- media.go
+++ media.go
@@ -1,32 +1,85 @@
 package goinsta
 
+// MediaInfo contains media information
+type MediaInfo struct {
+	media *Media
+
+	Status              string `json:"status"`
+	NumResults          int    `json:"num_results"`
+	AutoLoadMoreEnabled bool   `json:"auto_load_more_enabled"`
+	Items               []Item `json:"items"`
+	MoreAvailable       bool   `json:"more_available"`
+	CommentLikesEnabled bool   `json:"comment_likes_enabled"`
+}
+
+// MediaComments struct for get array of comments of a media
+type MediaComments struct {
+	media *Media
+
+	StatusResponse
+	NextMaxID           string            `json:"next_max_id"`
+	CommentLikesEnabled bool              `json:"comment_likes_enabled"`
+	Comments            []CommentResponse `json:"comments"`
+}
+
+// MediaLikers struct for get array of users that like a media
+type MediaLikers struct {
+	media *Media
+
+	StatusResponse
+	UserCount int    `json:"user_count"`
+	Users     []User `json:"users"`
+}
+
 // Media is media object representation
 //
 // Do not use concurrently
 type Media struct {
 	insta *Instagram
 
-	ID       string
-	Info     MediaInfo
-	Comments MediaComments
-	Likers   MediaLikers
+	ID string
+
+	Info     *MediaInfo
+	Comments *MediaComments
+	Likers   *MediaLikers
 }
 
-func NewMedia(insta *Instagram) {
-	return Media{insta: insta}
+// NewMedia returns new media
+func NewMedia(insta *Instagram) *Media {
+	media := &Media{insta: insta}
+	media.Info = MediaInfo{media: media}
+	media.Comments = MediaComments{media: media}
+	media.Likers = MediaLikers{media: media}
+	return media
 }
 
 // Get gets all media data (Likes, Likers, Comments)
-func (media *Media) Get(mediaID string) error {
-	media.ID = mediaID
-
-	media.Comments()
-	media.Likers()
-	media.Likes()
+//
+// id can be "" if Media.ID have been setted before.
+func (media *Media) Get(id string) {
+	if id != "" {
+		media.ID = id
+	}
+	media.Comments.Get()
+	media.Likers.Get()
+	media.Likes.Get()
 }
 
-// Comments collect all possible comments from media
-func (media *Media) Comments() error {
+// GetAsync does the same as Get but using goroutines
+func (media *Media) GetAsync(id string) {
+	if id != "" {
+		media.ID = id
+	}
+	go media.Comments.Get()
+	go media.Likers.Get()
+	go media.Likes.Get()
+}
+
+// Get collect comments.
+//
+// You can use Get once again to paginate.
+func (comments *MediaComments) Get() error {
+	media := comments.media
 	insta := media.insta
 	mediaID := media.ID
 
@@ -42,47 +95,48 @@ func (media *Media) Comments() error {
 		return err
 	}
 
-	err = json.Unmarshal(body, &media.Comments)
+	err = json.Unmarshal(body, comments)
 	return err
 }
 
-// MediaLikers return likers of a media , input is mediaid of a media
-func (media *Media) Likers() error {
-	body, err := insta.sendSimpleRequest("media/%s/likers/", mediaID)
+// Get fills likers structure.
+//
+// This structure cannot be paginated.
+func (likers *MediaLikers) Get() error {
+	body, err := insta.sendSimpleRequest("media/%s/likers/", likers.media.ID)
 	if err != nil {
 		return err
 	}
 
-	err = json.Unmarshal(body, &media.Likers)
-	return err
+	return json.Unmarshal(body, likers)
 }
 
-// MediaInfo return media information
-func (media *Media) Info() error {
+// Get fills Info with media info
+func (info *MediaInfo) Get() error {
+	media := info.media
 	insta := media.insta
 	result := MediaInfoResponse{}
 	mediaID := media.ID
 
 	req := acquireRequest()
-	req.args = fasthttp.AcquireArgs()
 	defer releaseRequest(req)
 
 	req.SetEndpoint(fmt.Sprintf("media/%s/info", mediaID))
 
-	data, err := insta.prepareData(map[string]interface{}{
-		"media_id": mediaID,
-	})
+	data, err := insta.prepareData(
+		map[string]interface{}{
+			"media_id": mediaID,
+		},
+	)
 	if err != nil {
-		return result, err
+		return err
 	}
 	req.SetData(generateSignature(data))
 
 	body, err := insta.sendRequest(req)
 	if err != nil {
-		return result, err
+		return err
 	}
 
-	err = json.Unmarshal(body, &result)
-
-	return result, err
+	return json.Unmarshal(body, info)
 }
